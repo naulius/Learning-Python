1f14dc3766bf3ae78e1d972e76b6d38ad1f8b4ff
Natalia Lucar <natalia@katteli.com>
2022-01-13 11:07:25 -0500

    Practising for-Loops and While-Loops

---
+++ Loops.py
@@ -0,0 +1,15 @@
+# Practicing for-loops
+
+# I want to repeat 20 times one phrase.
+print("---------------")
+print("\nExample: for-loop\n")
+for x in range(20):
+    print("Te amo Vitashka")
+
+# I want to give a condition to the code, which is to print from 1 to 10
+print("---------------")
+print("\nExample: while-loop\n")
+x = 0
+while x < 10:
+    x+=1
+    print(x)
