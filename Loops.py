f14737b9b0f1fbceb65a124a36cdeb4d2ad44b1a
Natalia Lucar <natalia@katteli.com>
2022-01-14 13:59:44 -0500

    Practising iterating a list, key and values of a dictionary

--- Loops.py
+++ Loops.py
@@ -13,3 +13,46 @@ x = 0
 while x < 10:
     x+=1
     print(x)
+
+# Iterate over a list and print each value
+print("----------------")
+print("Example: For-Loop iterating a list")
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for each_number in x:
+    print(each_number)
+
+# Iterate over values of a Dictionary
+print("-----------------")
+print("Example: For-Loop iterating over values of a dictionary")
+Words = {'Coco': 'Loco',
+         'Bro': 'Hermano',
+         'Sapo': 'Vivo'
+         }
+for value in Words.values():
+    print(value)
+
+# Iterate over keys of a dictionary
+print("-----------------")
+print("Example: For-Loop iterating over keys of a dictionary")
+Daughter_1 = {'Name': 'Elena', 'Age': 6, 'Born': 'Ottawa'
+              }
+Daughter_2 = {'Name': 'Iza', 'Age': 4, 'Born': 'Ottawa'
+              }
+Daughter_3 = {'Name': 'Kathy', 'Age': 18, 'Born': 'Texas'
+              }
+for key in list(Daughter_1.keys()) + list(Daughter_2.keys()) + list(Daughter_3.keys()):
+    print(key)
+print("------------------")
+print("Example_2 For-Loop iterating over key of a dictionary")
+Daughters = {
+    'Elena': {'Age': 6, 'Born': 'Ottawa'},
+    'Iza': {'Age': 4, 'Born': 'Ottawa'},
+    'Kathy': {'Age': 18, 'Born': 'Texas'}
+}
+for key in Daughters:
+    print(key)
+
+# Print all key-values pairs of dictionary
+print("------------------")
+print(Daughters.items())
+
