218fc6191f8dacfe93d1f2e6483079d14843ca01
Natalia Lucar <natalia@katteli.com>
2022-01-28 11:28:20 -0500

    Saving changes.

---
+++ Definition.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    print("add function called ", a, b)
+    return a + b
+
+r = add("x", "4")
+
+print(r)
+
+l
---
+++ Learning-Python-Course2/Learning-Basics.py
@@ -0,0 +1,18 @@
+# Use shift + enter to go to other cell
+# Learning some commands as:
+# cd: current directory
+# dir: content of my directory
+# cd .. : go back one step
+# clear : clear terminal
+# mv : change name of a directory or file
+# To run a code in the terminal: Python3 (name of file)
+# clt C: Interrupt a program
+# clt D : Terminate
+
+#Example to run a code in terminal:
+print(2+ 2)
+# Write python 3 name of the file enter
+print("Hello Vitaliy")
+
+
+
