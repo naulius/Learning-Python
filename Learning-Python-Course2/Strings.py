f3d101c72a2f77ba7800ff6527f86047a335eac5
Natalia Lucar <natalia@katteli.com>
2022-02-01 11:23:54 -0500

    Basic learning about variables and strings.

---
+++ Learning-Python-Course2/Strings.py
@@ -0,0 +1,30 @@
+# Strings area sequence of characters or words which are written in single or double quotes.
+# Example :
+"Hola"
+'No'
+
+# String are in an ordered sequence.We can use indexing and slicing.
+# Indexing: Use brackets [].Take a portion or character in an index position.
+# Example:
+# Characters :    h  e  l  l   o
+# Index :         0  1  2  3   4
+# Reverse Index:  0 -4 -3 -2  -1
+
+# Slicing: Use brackets [].Take a slice/part/multiple characters of the string.
+# The syntax is: [start,stop,step]
+# Start is the numerical index for the slice start.
+# Stop is the index that you want to take but not included.
+# Step is the size of the jump you take.
+# Spaces count as characters in the string.
+# Try not to mix characters in the same string, it can confuse Python.
+# Use backslash and n when you want to write in other cell.
+# Example:
+print("Hola \namigos todos")
+
+# You can also check the length of a string
+# Example:
+print(len("Hola amigos")) # It has 11 letters, it counts spaces.
+
+
+
+
---
+++ Learning-Python-Course2/Variables-Assignment.py
@@ -0,0 +1,21 @@
+# A variable is a data type with a name and value. A variable name can not be a number. It has to be a word or few words
+# For example:
+# my_dog = ["Joe", "Moma]. This is a list that has variable name,it can be changed at least in Python but not in other
+# program language.
+# with no space, instead we use the underscore symbol _.
+# We can not use other symbols but underscore.
+# It's recommended not to use special names use in Python like str,etc. Also is better to use lower case.
+# Example of variables:
+
+a = 5
+print(a+a)
+print(type(a)) # We can check the type of variable that we have, writing type and the name of the variable.
+# In this example the type of variable is an integer/int number.
+
+a = 3.5
+print(type(a)) # In this example the variable type is a float number.
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print(my_taxes)
