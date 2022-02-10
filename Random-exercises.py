8e7b11b0c4fcbb8e356fca447f520d46a940b604
Natalia Lucar <natalia@katteli.com>
2022-02-10 10:49:39 -0500

    Applying TestFlows to code.

--- Random-exercises.py
+++ Random-exercises.py
@@ -1,42 +1,44 @@
 from testflows.core import *
 
-with Example("If, else, conditions"):
-    x = 5
-    if x % 2 == 0:
-        print("Perfect")
-    else:
-        print("que mal")
+with Example("If, else, conditions, concatenate strings, while loops, for loops"):
 
-with Example("Concatenate strings"):
-    name = "Vitaliy"
-    greet = "Hola"
-    greeting = greet + " "  + name
-    print(greeting)
-    print(3 * "nat")
+    with Example("If, else, conditions"):
+        x = 5
+        if x % 2 == 0:
+            note("Perfect")
+        else:
+            note("que mal")
 
-with Example("While loops"):
-    n = 0
-    while(n < 5):
-         print(n)
-         n = n + 1
+    with Example("Concatenate strings"):
+        name = "Vitaliy"
+        greet = "Hola"
+        greeting = greet + " "  + name
+        note(greeting)
+        note(3 * "nat")
+
+    with Example("While loops"):
+        n = 0
+        while(n < 5):
+             note(n)
+             n = n + 1
 
 
-with Example("for loops"):
-    for n in range (5):
-        print(n)
+    with Example("for loops"):
+        for n in range (5):
+            note(n)
 
-    my_sum = 0
-    for i in range (7,10):
-        my_sum += i
-    print(my_sum)
+        my_sum = 0
+        for i in range (7,10):
+            my_sum += i
+        note(my_sum)
 
-with Example("for loops using break"):
-    my_sum = 0
-    for i in range(5, 11, 2):
-        my_sum += i
-        if my_sum == 5:
-            break
-            print(my_sum)
+    with Example("for loops using break"):
+        my_sum = 0
+        for i in range(5, 11, 2):
+            my_sum += i
+            if my_sum == 5:
+                break
+                note(my_sum)
 
 
 
