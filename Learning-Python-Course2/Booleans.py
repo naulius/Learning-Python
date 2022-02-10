cccfd11551219f25f832daca914d29a1b4a30df5
Natalia Lucar <natalia@katteli.com>
2022-02-10 11:38:13 -0500

    Learning about Booleans.

---
+++ Learning-Python-Course2/Booleans.py
@@ -0,0 +1,15 @@
+# Booleans give a TRUE or FALSE statement.Write the first letter in capital for True and False.
+# Example:
+True
+print(True)
+False
+print(False)
+x = type(False)
+print(x)
+y = 1 > 2
+print(y) # It tells you that statement is False.
+z = 1 == 1
+print(z)
+b = None
+print(b) # Returns None.
+
