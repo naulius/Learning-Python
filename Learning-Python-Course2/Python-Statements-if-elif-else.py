34cebc90002733742f9eed8f06336ec94cc07832
Natalia Lucar <natalia@katteli.com>
2022-02-22 11:30:59 -0500

    Learning python statements if, elif, else.

---
+++ Learning-Python-Course2/Python-Statements-if-elif-else.py
@@ -0,0 +1,36 @@
+# Python Statements If, Elif and Else.
+# Control Flows helps us to execute certain part of code when we want to.
+# To control flow we use some keywords: IF, ELIF, ELSE.
+# Index for IF/ELSE: Use colons and indentation.
+
+# if some condition:
+      # Execute some code
+# elif some other_condition:
+      # Do something different
+# else:
+      # Do something else
+
+# Example:
+x = 4 > 3
+if x:
+    print("great")
+elif x == 5:
+    print("not_bad")
+else:
+    print("ok")
+
+name = "Vitaliy"
+if name == "Natalia":
+    print("Hi Nata")
+elif name == "Vitaliy":
+    print("Hi Vitashka")
+else:
+    print("What is your name?")
+
+
+
+
+
+
+
+
