898c34361b9dc785290718af66182dd9253d27d1
Natalia Lucar <natalia@katteli.com>
2022-03-04 11:06:57 -0500

    General learning about Methods and how to read the documentation of those methods.

---
+++ Learning-Python-Course2/Methods_Python documentation.py
@@ -0,0 +1,9 @@
+# Methods are function that are built in to objects.
+# Example:
+mylist = [1,2,3]
+mylist.append(4)  # Append is a method that add an element.
+print(mylist)
+mylist.pop(0)  # Pop is another method that remove an element.
+print(mylist)
+help(mylist.insert)  # It gets the documentation for a particular method.
+
