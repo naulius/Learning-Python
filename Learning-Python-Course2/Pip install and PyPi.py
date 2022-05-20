355776d5320bb973cb6915765f163b563bc6feda
Natalia Lucar <natalia@katteli.com>
2022-05-20 10:34:17 -0400

    Learning about pip and PyPi.

---
+++ Learning-Python-Course2/Pip install and PyPi.py
@@ -0,0 +1,4 @@
+# PyPi is a repository for open-source third party Python packages.
+# There are other many libraries available that people open-sourced and shared on PyPi.
+# We can use pip install at the command line to install these packages.
+# Pip is a simple way to download packages at your command line directly from the PyPi repository.
