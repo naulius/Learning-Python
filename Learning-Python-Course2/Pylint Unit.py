04fc3d62b3468a558fc4481916dabd9edd25abe2
Natalia Lucar <natalia@katteli.com>
2022-08-26 10:08:36 -0400

    Learning about Pylint Unit.

---
+++ Learning-Python-Course2/Pylint Unit.py
@@ -0,0 +1,3 @@
+# Pylint: This is a library that looks at your code and reports back possible issues.
+# UnitTest: This is built-in library that will allow to test your own programs and check
+# you are getting your desired outputs.
