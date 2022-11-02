da700863292e93e280f04cbb4bc8c31376c7a3cc
Natalia Lucar <natalia@katteli.com>
2022-11-02 10:54:57 -0400

    Learning Daytime_Module.

---
+++ Learning-Python-Course2/Daytime_Module.py
@@ -0,0 +1,17 @@
+import datetime
+mytime = datetime.time(2,20)
+print(mytime.minute)
+print(mytime.hour)
+print(mytime)
+
+type(mytime)
+datetime.time
+today = datetime.date.today()
+print(today)
+print(today.year)
+
+from datetime import datetime
+mydatetime = datetime(2022,11,2,10,47,1)
+print(mydatetime)
+
+
