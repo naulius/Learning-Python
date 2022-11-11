c2dca64e9b0617fffea7871daf725c9ee9fb5b16
Natalia Lucar <natalia@katteli.com>
2022-11-11 11:02:30 -0500

    Learning about Regular expressions in Python.

---
+++ Learning-Python-Course2/Regular_expressions.py
@@ -0,0 +1,22 @@
+text = "the agent's phone number is 408-555-1234. Call soon"
+"phone" in text
+import re
+pattern = "phone"
+print(re.search(pattern, text))
+pattern = "not on text"
+re.search(pattern, text)
+pattern = "phone"
+match = re.search(pattern, text)
+print(match)
+print(match.span())
+print(match.start())
+print(match.end())
+text = "my phone once, my phone twice"
+match= re.search("phone", text)
+print(match)
+matches = re.findall("phone", text)
+print(matches)
+print(len(matches))
+
+for match in re.finditer("phone", text):
+    print(match.span())
